d4183b27ecde4ceca053ab90c6add699773ec096
Freddy <freddyjohnsamuel@gmail.com>
2019-02-20 03:23:35 -0800

    Config

--- src/main/java/com/boa/kyc/eureka/HysterixApplication.java
+++ src/main/java/com/boa/kyc/eureka/HysterixApplication.java
@@ -2,10 +2,13 @@ package com.boa.kyc.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class HysterixApplication {
 
